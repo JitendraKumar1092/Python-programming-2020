647cd9f29491a52a726fefd14bf763d22b5926c3
JitendraKumar1092 <76446860+JitendraKumar1092@users.noreply.github.com>
2021-03-15 09:51:46 +0000

    odd even

---
+++ oddeven.py
@@ -0,0 +1,12 @@
+'''
+wap to check odd even
+wap to identify the character with upr lwr digit sp char'''
+
+
+a=int(input())
+if a%2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
+
+        
