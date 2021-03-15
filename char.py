9881b1557ed14ee142b5bb98bdd43f106cb79b3d
JitendraKumar1092 <76446860+JitendraKumar1092@users.noreply.github.com>
2021-03-15 09:50:55 +0000

    odd_even program

---
+++ char.py
@@ -0,0 +1,10 @@
+a = raw_input()
+if (a<='Z' and a>='A'):
+    print('uppercase')
+elif (a <= 'z' and a >= 'a' ):
+    print('lower case')
+elif (
+    a >= '0' and a<='9' ) :
+    print('it is a digit')
+else :
+    print('special character')
