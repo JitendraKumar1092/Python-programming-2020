0a3edd1630d16003caad5e3740ade140bb2c1d17
JitendraKumar1092 <76446860+JitendraKumar1092@users.noreply.github.com>
2021-03-22 15:49:47 +0530

    Update reverse_num.py

--- reverse_num.py
+++ reverse_num.py
@@ -1,2 +1,15 @@
-num = input("enter the number to reverse it :- ")
-print('reverse number is',num[::-1])
+text = input()
+lenth= len(text)
+stir = text.lower()
+vc=0
+cc=0
+sc=0
+for i in range (lenth):
+    if stir[i] in ('a','e','i','o','u'):
+        vc= vc + 1 
+    elif stir[i] >= 'a' and stir[i] <= 'z' :
+        cc = cc + 1 
+    else:
+        sc =  sc +1 
+print('vowels = ',vc, 'consonant = ',cc,'special characters =',sc)
+
