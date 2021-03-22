0c762698108fa36bda71fa910d5321f4a1610d74
JitendraKumar1092 <76446860+JitendraKumar1092@users.noreply.github.com>
2021-03-22 15:37:12 +0530

    Create char_type_count.py

---
+++ char_type_count.py
@@ -0,0 +1,14 @@
+text = input()
+lenth= len(text)
+stir = text.lower()
+vc=0
+cc=0
+sc=0
+for i in range (lenth):
+    if stir[i] in ('a','e','i','o','u'):
+        vc= vc + 1 
+    elif stir[i] >= 'a' and stir[i] <= 'z' :
+        cc = cc + 1 
+    else:
+        sc =  sc +1 
+print('vowels = ',vc, 'consonant = ',cc,'special characters =',sc)
